288d0c1bf60bd92fdebd2e665cdd56787b750697
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-26 18:07:17 +0600

    react reacp module complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+// const friendName =[{name:"sakib", age: '22'},{name:'rakib', age:'25'},{name:"kashem",age:'27'},{name:'hashem',age: '22'},{name:'Ankon',age:'10'}];
+const [users,setUser] = useState([])
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => setUser(data))
+},[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MovieCounter></MovieCounter>
+        <TotalMovies></TotalMovies>
+        {
+         users.map(user => <Friend name={user.name} key={user.id}></Friend>)
+        }
+        
       </header>
     </div>
   );
+}
+function MovieCounter(){
+  const [count,setCount]=useState(0);
+
+  return (
+    <div>
+      <button onClick={() =>setCount(count+1)}>Add Movie </button>
+      <h3>Numbers of movie: {count}</h3>
+      <TotalMovies movies={count}></TotalMovies>
+    </div>
+  )
+}
+function TotalMovies(props){
+  return (
+    <div>
+       <h4>total Movie: {props.movies}</h4>
+  
+    </div>
+  )
+}
+function Friend(props){
+  
+  const friendStyle={
+    backgroundColor: 'grey',
+    borderRadius: '10px',
+    width: '350px',
+    height: '250px',
+    margin: '5px',
+    
+  }
+  return <div style={friendStyle}>
+      <h3>Name: {props.name}</h3>
+      <h5>Age:</h5>
+    </div>
+  
 }
 
 export default App;
